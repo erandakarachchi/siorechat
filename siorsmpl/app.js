21812dceb52eb8490aba7dc5239cbe101bcea21a
erandakarachchi <erandakarachchi@gmail.com>
2020-05-30 22:34:34 +0530

    Initial commit of react app with s io installed

---
+++ siorsmpl/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http")
+const socketIO = require("socket.io");
+
+
+const port = process.env.port | 4001;
+const index = require("./routes/index")
+
+const app = express()
+app.use(index)
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+let interval;
+io.on('connection', (socket => {
+    console.log("New Client Connected")
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on("disconnect", () => {
+        console.log("Client Disconnected");
+        clearInterval(interval);
+    })
+}))
+
+const getApiAndEmit = socket => {
+    const response = new Date();
+    socket.emit("FromAPI", response); 
+}
+
+server.listen(port,()=>console.log(`Server Listing to port ${port}`))
